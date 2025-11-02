edf422c199df5f25e23ae6d8d18fb8bb1ab68479
chacha86 <taejincha0619@gmail.com>
2025-11-03 07:48:02 +0900

    jvmRecord

--- backend/src/main/java/com/back/domain/member/member/dto/MemberDto.kt
+++ backend/src/main/java/com/back/domain/member/member/dto/MemberDto.kt
@@ -3,7 +3,9 @@ package com.back.domain.member.member.dto
 import com.back.domain.member.member.entity.Member
 import java.time.LocalDateTime
 
-@JvmRecord
+// @JvmRecord 지우면 몇몇 테스트가 실패한다.
+// 코틀린 직렬화시에는 is를 제거 하고 getter를 사용하기 때문에 isAdmin -> admin 으로 변환된다.
+// 이를 유지하기 위해서는 @JvmRecord 어노테이션을 사용해야 한다.
 data class MemberDto(
     val id: Long,
     val createDate: LocalDateTime,
