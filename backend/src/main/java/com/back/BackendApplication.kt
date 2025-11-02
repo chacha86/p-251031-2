bf8aad2053a60ae9287e3a9f78aecf5b4396e88e
chacha86 <taejincha0619@gmail.com>
2025-11-03 07:23:59 +0900

    plugin.spring

--- backend/src/main/java/com/back/BackendApplication.kt
+++ backend/src/main/java/com/back/BackendApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-open class BackApplication // Configuration 클래스가 proxy 처리 되므로 final 클래스일 수 없다. open class 여야 한다.
+it class BackApplication // Configuration 클래스가 proxy 처리 되므로 final 클래스일 수 없다. open class 여야 한다.
 
 fun main(args: Array<String>) {
     runApplication<BackApplication>(*args)
