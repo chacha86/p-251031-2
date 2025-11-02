d97c7a91c4416e62dc1a25c5fe6590dc6cf76424
chacha86 <taejincha0619@gmail.com>
2025-11-03 07:22:51 +0900

    open add

--- backend/src/main/java/com/back/BackendApplication.kt
+++ backend/src/main/java/com/back/BackendApplication.kt
@@ -1,14 +1,13 @@
 package com.back
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-object BackendApplication {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(BackendApplication::class.java, *args)
-    }
+open class BackApplication // Configuration 클래스가 proxy 처리 되므로 final 클래스일 수 없다. open class 여야 한다.
+
+fun main(args: Array<String>) {
+    runApplication<BackApplication>(*args)
 }
