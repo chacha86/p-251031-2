b8fa397f98c12fdc3865f5ddd1966d25b87418e2
chacha86 <taejincha0619@gmail.com>
2025-11-03 11:50:54 +0900

    RsData - 1

---
+++ backend/src/main/java/com/back/global/rsData/RsData.kt
@@ -0,0 +1,22 @@
+package com.back.global.rsData
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import lombok.AllArgsConstructor
+import lombok.Getter
+
+@AllArgsConstructor
+@Getter
+class RsData<T> @JvmOverloads constructor (
+    val resultCode: String,
+    val msg: String,
+    val data: T? = null
+) {
+
+    @get:JsonIgnore
+    val statusCode: Int
+        get() {
+            val statusCode =
+                resultCode.split("-".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+            return statusCode.toInt()
+        }
+}
